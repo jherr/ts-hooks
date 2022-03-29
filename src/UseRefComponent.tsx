5eca654733f53304b8cb833ec6436c77175b7e22
Demian Caldelas <denik.works@protonmail.com>
2022-03-29 00:56:17 -0300

    Expand on an useRef simple use case

--- src/UseRefComponent.tsx
+++ src/UseRefComponent.tsx
@@ -3,7 +3,18 @@ import { useRef } from "react";
 function UseRefComponent() {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  return <input ref={inputRef} />;
+  return (
+    <div>
+      <input ref={inputRef} />;
+      <button
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+      >
+        Focus on input
+      </button>
+    </div>
+  );
 }
 
 export default UseRefComponent;
